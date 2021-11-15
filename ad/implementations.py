3de2f348661371f07fa4964b23b99edd8ef078b3
lerdem <foo@mail.com>
2021-11-15 17:02:08 +0200

    refactor: rename

--- ad/implementations.py
+++ ad/implementations.py
@@ -13,8 +13,10 @@ from ad.core.usecases.create_base_ads import create
 from ad.core.usecases.create_detail_ad import create_detail
 from ad.core.usecases.get_ads import get
 
-get_base = partial(get, repo=GetRepoCsv(), presenter=FeedPresenter())
-get_detail = partial(get, repo=GetDetailedRepoCsv(), presenter=FeedDetailedPresenter())
+get_base_ads = partial(get, repo=GetRepoCsv(), presenter=FeedPresenter())
+get_detail_ads = partial(
+    get, repo=GetDetailedRepoCsv(), presenter=FeedDetailedPresenter()
+)
 ad_detail_uploader = partial(
     create_detail, repository=DetailRepoCsv(), provider=GetItemProvider()
 )
--- app.py
+++ app.py
@@ -1,18 +1,18 @@
 from flask import Flask, Response, request
-from ad.implementations import get_base, get_detail
+from ad.implementations import get_base_ads, get_detail_ads
 
 app = Flask(__name__)
 
 
 @app.route('/rss')
 def hello_world():
-    data = get_base(tag=request.args.get('tag'))
+    data = get_base_ads(tag=request.args.get('tag'))
     return Response(data, headers={'Content-Type': 'application/rss+xml'})
 
 
 @app.route('/detail-rss')
 def detail_rss():
-    data = get_detail(tag=request.args.get('tag'))
+    data = get_detail_ads(tag=request.args.get('tag'))
     return Response(data, headers={'Content-Type': 'application/rss+xml'})
 
 
