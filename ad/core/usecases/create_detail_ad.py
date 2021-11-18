3f7f6796c7257080c38222ec72bc703b002a56c0
lerdem <foo@mail.com>
2021-11-18 16:43:30 +0200

    refactor: replace manual DI with framework

--- ad/core/usecases/create_detail_ad.py
+++ ad/core/usecases/create_detail_ad.py
@@ -1,17 +1,24 @@
+from dataclasses import dataclass
+
 from ad.core.adapters.provider import DetailProvider
 from ad.core.adapters.repository import DetailRepo
 from ad.core.entities import DetailedAd
 
 
-def create_detail(ad_id: str, repository: DetailRepo, provider: DetailProvider):
-    base_ad = repository.get_base_ad_by_id(ad_id)
-    raw = provider.get_raw(base_ad.url)
-    detailed_ad = DetailedAd(
-        image_urls=raw[0],
-        external_id=raw[1],
-        description=raw[2],
-        name=raw[3],
-        **base_ad.dict()
-    )
+@dataclass
+class CreateDetailUseCase:
+    _repository: DetailRepo
+    _provider: DetailProvider
 
-    repository.save(detailed_ad)
+    def __call__(self, ad_id: str) -> None:
+        base_ad = self._repository.get_base_ad_by_id(ad_id)
+        raw = self._provider.get_raw(base_ad.url)
+        detailed_ad = DetailedAd(
+            image_urls=raw[0],
+            external_id=raw[1],
+            description=raw[2],
+            name=raw[3],
+            **base_ad.dict()
+        )
+
+        self._repository.save(detailed_ad)
--- ad/implementations.py
+++ ad/implementations.py
@@ -10,18 +10,15 @@ from ad.adapters.repository import (
     CreateRepoCsv,
     ConfigRepoJson,
 )
-from ad.core.adapters.provider import CreateProvider
-from ad.core.adapters.repository import CreateRepo, ConfigRepo
+from ad.core.adapters.provider import CreateProvider, DetailProvider
+from ad.core.adapters.repository import CreateRepo, ConfigRepo, DetailRepo
 from ad.core.usecases.create_base_ads import CreateAdsUseCase
-from ad.core.usecases.create_detail_ad import create_detail
+from ad.core.usecases.create_detail_ad import CreateDetailUseCase
 from ad.core.usecases.get_ads import get
 
 get_base_ads = partial(get, repo=GetRepoCsv(), presenter=FeedPresenter())
 get_detail_ads = partial(
     get, repo=GetDetailedRepoCsv(), presenter=FeedDetailedPresenter()
-)
-ad_detail_uploader = partial(
-    create_detail, repository=DetailRepoCsv(), provider=GetItemProvider()
 )
 
 
@@ -31,3 +28,9 @@ container.register(CreateProvider, CreateProviderOlx)
 container.register(ConfigRepo, ConfigRepoJson)
 container.register(CreateAdsUseCase)
 ads_creator = container.resolve(CreateAdsUseCase)
+
+
+container.register(DetailRepo, DetailRepoCsv)
+container.register(DetailProvider, GetItemProvider)
+container.register(CreateDetailUseCase)
+ad_detail_uploader = container.resolve(CreateDetailUseCase)
