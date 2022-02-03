bfaef9550c66329cbd6811124a13913c8a2b3706
lerdem <foo@mail.com>
2022-02-03 16:10:40 +0200

    feat: allow to skip not parsed ads

--- ad/upload_ads.py
+++ ad/upload_ads.py
@@ -4,4 +4,7 @@ if __name__ == '__main__':
     new_ad_ids = ads_creator()
     print(new_ad_ids)
     for _id in new_ad_ids:
-        ad_detail_uploader(ad_id=_id)
+        try:
+            ad_detail_uploader(ad_id=_id)
+        except Exception as e:
+            print(f'Error with {_id}, {e}')
