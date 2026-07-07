27b1d0cc37a8eb3ceec6fcc3f2f40b00b43e3365
lerdem <foo@mail.com>
2026-07-07 14:37:53 +0300

    feat(fix): default sorting on publication date

--- ad/core/usecases/get_ads.py
+++ ad/core/usecases/get_ads.py
@@ -22,7 +22,7 @@ class GetAdsUseCase:
         ads: List[DetailedAd] = self._repo.get_by_tag(tag) if tag is not None else self._repo.get_all()
         if stop_words:
             ads = self._exclude_stop_words_from_ads(stop_words, ads)
-        last_ads = sorted(ads, key=lambda x: x.parse_date, reverse=True)[:max_num_ads_show]
+        last_ads = sorted(ads, key=lambda x: x.publication_date, reverse=True)[:max_num_ads_show]
         return self._presenter.present(last_ads)
 
     @staticmethod
