7fbb4e98d780983293b42c6346f396987ea387de
lerdem <foo@mail.com>
2026-08-01 17:46:38 +0300

    fead(build): move upload loop to bash

--- ad/upload_ads.py
+++ ad/upload_ads.py
@@ -1,6 +1,5 @@
 import sys
-from time import sleep, time
-from random import randint
+from time import time
 
 from ad.core.errors import UseCaseError
 from ad.implementations import ad_detail_uploader, create_ads_and_notify
@@ -9,28 +8,22 @@ from ad.logger import logger
 
 def _upload_job():
     logger.debug('UPLOADER: started')
-    while True:
-        started = time()
-        try:
-            new_ad_ids = create_ads_and_notify()
-        except UseCaseError as e:
-            logger.error(f'UPLOADER: {e}')
-            logger.debug(f'UPLOADER: duration {time() - started}')
-        else:
-            logger.debug(f'UPLOADER: Загружены ads {new_ad_ids}')
-            for _id in new_ad_ids:
-                try:
-                    ad_detail_uploader(ad_id=_id)
-                except Exception as e:
-                    logger.error(
-                        f'UPLOADER: Error with {_id}, {e}'
-                    )
+    started = time()
+    try:
+        new_ad_ids = create_ads_and_notify()
+    except UseCaseError as e:
+        logger.error(f'UPLOADER: {e}')
         logger.debug(f'UPLOADER: duration {time() - started}')
-        time_to_wait = randint(60, 120)
-        logger.debug(
-            f'UPLOADER: waiting before upload from olx {time_to_wait} seconds'
-        )
-        sleep(time_to_wait)
+    else:
+        logger.debug(f'UPLOADER: Загружены ads {new_ad_ids}')
+        for _id in new_ad_ids:
+            try:
+                ad_detail_uploader(ad_id=_id)
+            except Exception as e:
+                logger.error(
+                    f'UPLOADER: Error with {_id}, {e}'
+                )
+    logger.debug(f'UPLOADER: duration {time() - started}')
 
 
 if __name__ == '__main__':
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
-python -m ad.upload_ads &
+(
+  while true; do
+    python -m ad.upload_ads
+    # Random sleep between 60 and 120 seconds in shell
+    SLEEP_TIME=$((60 + RANDOM % 61))
+    echo "UPLOADER: sleeping for ${SLEEP_TIME}s"
+    sleep $SLEEP_TIME
+  done
+) &
 python -m ad.bulk_deactivation_inactive_ads &
 exec gunicorn --threads 4 --bind 0.0.0.0:8000 app:app
